50ca8d1c6bbd9a85c6ba0cd6754fd431202c7c23
TheDonten <32191011+TheDonten@users.noreply.github.com>
2018-04-13 14:21:31 +0300

    Update vector.cpp

--- tests/vector.cpp
+++ tests/vector.cpp
@@ -9,27 +9,38 @@ TEST_CASE("create queue_t"){
  REQUIRE(q1.tail_() == nullptr);
  }
 TEST_CASE("push queue"){
-queue_t<int> q1;
+ queue_t<int> q1;
  q1.push(1);
  q1.push(2);
  q1.push(3);
  std::string out{"1---2---3---"};
-   std::ostringstream stream;
-  q1.write(stream,q1.head_());
-  REQUIRE(stream.str() == out);
+ std::ostringstream stream;
+ q1.write(stream,q1.head_());
+ REQUIRE(stream.str() == out);
 }
 TEST_CASE("pop_back"){
 queue_t<int> q1;
 q1.push(1);
 q1.push(2);
- REQUIRE( q1.pop() == 1 );
-	REQUIRE( q1.pop() == 2 );
-	
+REQUIRE( q1.pop() == 1 );
+REQUIRE( q1.pop() == 2 );	
 }
 TEST_CASE("error pop"){
  queue_t<int> q1;
  REQUIRE_THROWS_AS( queue.pop(), std::logic_error );
 }
 TEST_CASE(" q1=q2"){
- 
- }
+  queue_t<int> q1;
+  queue_t<int> q2;
+  std::string out{"5---6---7---"};
+  q1.push(1);
+  q1.push(2);
+  q1.push(3);
+  q2.push(5);
+  q2.push(6);
+  q2.push(7);
+  q1 = q2;
+  std::ostringstream stream;
+  q1.write(stream,q1.head_());
+  REQUIRE(stream.str() == out);
+  }
