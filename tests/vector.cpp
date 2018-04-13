018bfd2d9469fbe36d1d0ef02a913b49c88b8f28
TheDonten <32191011+TheDonten@users.noreply.github.com>
2018-04-13 13:21:08 +0300

    Update vector.cpp

--- tests/vector.cpp
+++ tests/vector.cpp
@@ -3,7 +3,21 @@
 
 #include "vector.hpp"
 
-
+TEST_CASE("create queue_t){
+ queue_t<int> q1;
+ REQUIRE(q1.head_() == nullptr);    
+ REQUIRE(q1.tail_() == nullptr);
+ }
+TEST_CASE("push queue"){
+queue_t<int> q1;
+ q1.push(1);
+ q2.push(2);
+ q3.push(3);
+ std::string out{1---2---3---};
+   std::ostringstream stream;
+  q1.write(stream,q1.head_());
+  REQUIRE(stream.str() == out);
+}
 
 
 
